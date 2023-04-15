b7ad8223d0690e037632b67d2b643c22c47b1c2c
Alexander <73365442+SashaMKB@users.noreply.github.com>
2023-04-15 09:55:30 +0300

    last version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +12,9 @@ public class Main {
             int numberOfAirports = 0;
             System.out.println("Enter filter:");
             String filter = airport.nextLine().replaceAll("'", "\"");
-
+            if (filter.contains("!exit")) {
+                break;
+            }
             System.out.println("Enter airport:");
             String name = airport.nextLine();
             if (name.contains("!exit")) {
@@ -24,7 +24,6 @@ public class Main {
                 reader = new BufferedReader(new FileReader("../airports.csv"));
                 String line;
                 long start = System.currentTimeMillis();
-                int i = 0;
                 while ((line = reader.readLine()) != null) {
                     String[] items = line.split(",");
                     if (items[1].toLowerCase().startsWith('"' + name.toLowerCase())) {
